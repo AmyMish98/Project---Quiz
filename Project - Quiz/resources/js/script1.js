d39583d834055f0c33b436919b2eab8c6f749939
AmyMish98 <31150661+AmyMish98@users.noreply.github.com>
2018-10-16 12:30:18 +0530

    Add files via upload

---
+++ Project - Quiz/resources/js/script1.js
@@ -0,0 +1,31 @@
+function Func(){
+var score=0;
+
+var x = document.getElementsByClassName('Q1');
+if (x[0].checked == true) {
+     score=score+5; 
+}
+
+x = document.getElementsByClassName('Q2');
+if (x[3].checked == true) {
+     score=score+5; 
+}
+
+x = document.getElementsByClassName('Q3');
+if (x[1].checked == true) {
+     score=score+5; 
+}
+
+x = document.getElementsByClassName('Q4');
+if (x[0].checked == true) {
+     score=score+5; 
+}
+
+x = document.getElementsByClassName('Q5');
+if (x[2].checked == true) {
+     score=score+5; 
+}
+
+alert("Your score is: " + score);
+window.location.reload();
+}
